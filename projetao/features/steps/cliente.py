10724f0d935ceb486108607858e4168cce2e4bf0
battist <felipebattist@gmail.com>
2022-04-01 16:16:17 -0300

    Teste de aceitação para função delete funcionando.

--- projetao/features/steps/cliente.py
+++ projetao/features/steps/cliente.py
@@ -22,13 +22,13 @@ def step_impl(context):
 @given("I fill all fields")
 def step_impl(context):
     nome = context.browser.find_element_by_name("nome")
-    nome.send_keys("Testador2")
+    nome.send_keys("Testador")
     email = context.browser.find_element_by_name("email")
-    email.send_keys("email001010231@email.com12")
+    email.send_keys("cadastrar3@email.com")
     contato = context.browser.find_element_by_name("contato")
     contato.send_keys("00078795455")
     cpf_cnpj = context.browser.find_element_by_name("cpf_cnpj")
-    cpf_cnpj.send_keys("00000000000")
+    cpf_cnpj.send_keys("45265235261")
     senha = context.browser.find_element_by_name("senha")
     senha.send_keys("123456")
 
@@ -40,11 +40,14 @@ def step_impl(context):
 @then("I create my profile")
 def step_impl(context):
     cliente = Cliente.objects.all()
-    assert cliente.filter(email="email001010231@email.com12")!=[]
+    assert cliente.filter(email="cadastrar3@email.com")!=[]
 
 @then("go to my Profile View")
 def step_impl(context):
     assert context.browser.title=="Perfil"
+
+
+#=================================================================================================================
    
 @then("stay in the Cadastrar Cliente View")
 def step_impl(context):
@@ -53,9 +56,9 @@ def step_impl(context):
 @given("I do not fill all fields")
 def step_impl(context):
     nome = context.browser.find_element_by_name("nome")
-    nome.send_keys("Testador1")
+    nome.send_keys("Testador_Cadastro_Falso")
     email = context.browser.find_element_by_name("email")
-    email.send_keys("email001010@email.com1")
+    email.send_keys("cadastro_falso30email.com")
     contato = context.browser.find_element_by_name("contato")
     contato.send_keys("0123456")
     senha = context.browser.find_element_by_name("senha")
@@ -65,7 +68,44 @@ def step_impl(context):
 def step_impl(context):
     try:
         cliente = Cliente.objects.all()
-        cliente.filter(email="email001010@email.com1")
+        cliente.filter(email="cadastro_falso30email.com")
         assert True
     except:
         assert False
+
+
+#=========================================================================================================
+@Given("I am registered user")
+def step_impl(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("http://127.0.0.1:8000/")
+    nome = context.browser.find_element_by_name("nome")
+    nome.send_keys("Testador_Delete")
+    email = context.browser.find_element_by_name("email")
+    email.send_keys("delete3@email.com")
+    contato = context.browser.find_element_by_name("contato")
+    contato.send_keys("00078795455")
+    cpf_cnpj = context.browser.find_element_by_name("cpf_cnpj")
+    cpf_cnpj.send_keys("62165865817")
+    senha = context.browser.find_element_by_name("senha")
+    senha.send_keys("123456")
+    button = context.browser.find_element_by_name("cadastro")
+    button.submit()
+@Given("I am at the profile view")
+def step_impl(context):
+    assert context.browser.title == "Perfil"
+
+@When("I click Desativar Conta")
+def step_impl(context):
+    button = context.browser.find_element_by_name("desativar")
+    button.submit()
+
+@Then("I deactivate my profile")
+def step_impl(context):
+    cliente = Cliente.objects.all()
+    assert cliente.filter(email="delete3@email.com", active=False) != []
+
+@Then("go to Cadastrar Cliente view")
+def step_impl(context):
+    assert context.browser.title == "Cadastrar-se"
+

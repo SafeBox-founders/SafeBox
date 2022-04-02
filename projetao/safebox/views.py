4b960390519024ceea093c1ccb89aaef9a4eae4e
cleovaldo <cleovaldobcc@gmail.com>
2022-04-01 22:06:29 -0300

    View de Editar perfil de Cliente.

--- projetao/safebox/views.py
+++ projetao/safebox/views.py
@@ -20,12 +20,37 @@ def cliente_detail_view(request, email):
     context = {}
     context["data"] = Cliente.objects.get(email=email)
     if request.method == 'POST':
-        action = request.POST.get('desativar')
-        if action == 'Desativar Conta':
+        action_desativar = request.POST.get('desativar')
+        action_editar = request.POST.get('editar')
+        if action_desativar == 'Desativar Conta':
             context['data'].deactivate()
             context['data'].save()
+            return redirect('cadastrar')
 
-        return redirect('cadastrar')
+        if action_editar == 'Editar Conta':
+            context['data'].reactivate()
+            context['data'].save()
+            return redirect('editar',context['data'].get_email())
 
     return render(request, "cliente_detail_view.html", context)
+
+def cliente_edit_view(request, email):
+    context = {}
+    context["data"] = Cliente.objects.get(email=email)
+
+    # pass the object as instance in form
+    form = ClienteForm(request.POST or None, instance=context['data'])
+
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        email = request.POST['email']
+        return redirect('visualizar',email)
+
+    # add form dictionary to context
+    context["form"] = form
+
+    return render(request, "cliente_edit_view.html", context)
+
 

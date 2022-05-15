49dd873ef4a398bb958c68364f2231579bccf45e
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2022-05-14 23:16:14 -0300

    Validações em contato e cpf_cnpj

--- projetao/safebox/forms.py
+++ projetao/safebox/forms.py
@@ -1,11 +1,33 @@
 from unicodedata import name
 from django import forms
 from .models import Ambiente, Assinatura, Camera, Cliente, Plano, BoundingBox, Relatorio
+from django.core.exceptions import ValidationError
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ["nome", "email", "contato", "cpf_cnpj", "senha"]
+        widgets = {
+            "cpf_cnpj" : forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[0-9]+', 'title':'Digite somente números'}),
+            "contato" : forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[0-9]+', 'title':'Digite somente números'})
+        }
+
+    def clean_contato(self):
+        contato = self.cleaned_data['contato']
+        min_length = 8
+        max_length = 11
+
+        if len(contato) < min_length or len(contato) > max_length:
+            raise ValidationError("Contato residencial ou celular deve ter entre %s e %s números!" %(min_length, max_length))
+
+    def clean_cpf_cnpj(self):
+        value = self.cleaned_data['cpf_cnpj']
+        min_length = 11
+        if len(str(value)) < min_length:
+            raise ValidationError(
+                "CPF ou CNPJ deve ter no mínimo 11 caracteres! '%s' tem apenas %s." %(value, len(value)))
+        else:
+            return value
 
 class ClienteLoginForm(forms.Form):
     email = forms.CharField(max_length=255, widget=forms.EmailInput)

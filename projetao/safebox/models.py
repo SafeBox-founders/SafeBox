db3f2e5d45c4a609f2a50ce6346c6df0f19fa413
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2022-05-15 15:27:31 -0300

    Validações em contato e cpf_cnpj

--- projetao/safebox/models.py
+++ projetao/safebox/models.py
@@ -5,7 +5,7 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=255,null=False)
     email = models.EmailField(max_length=255,null=False,unique=True)
     contato = models.CharField(max_length=11,null=False)
-    cpf_cnpj = models.CharField(max_length=11,null=False,unique=True)
+    cpf_cnpj = models.CharField(max_length=14,null=False,unique=True)
     senha = models.CharField(max_length=12,null=False)
     joined = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
